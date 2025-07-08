cf25bab384b54fbf9410eb602e5324cd770e1a83
JaHyeok <jhku2433@gmail.com>
2025-07-08 17:46:25 +0900

    add .gitignore and initial project structure

---
+++ src/main/java/com/TripRider/TripRider/TripRiderApplication.java
@@ -0,0 +1,13 @@
+package com.TripRider.TripRider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripRiderApplication.class, args);
+	}
+
+}
