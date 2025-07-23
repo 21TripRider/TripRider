d45fe9042b4f60b24ecb4488a26a7d299f11285d
davidcho0701 <davidcho0701@gmail.com>
2025-07-23 14:55:32 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:triprider/screens/HomeScreen.dart';
-import 'package:triprider/screens/Login/CreateAccountScreen.dart';
-import 'package:triprider/screens/Login/LoginScreen.dart';
-import 'package:triprider/screens/Login/WelcomeScreen.dart';
+import 'screens/nickname_input_screen.dart'; // ✅ 추가
 
-void main(){
-  runApp(
-    MaterialApp(
-      home: Loginscreen(),
-    )
-  );
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Triprider UI Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Apple SD Gothic Neo',
+      ),
+      home: const NicknameInputScreen(), // ✅ 시작화면 설정
+    );
+  }
 }
