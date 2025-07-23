09d784df83102c9877c6c288be3a45037db78e47
davidcho0701 <davidcho0701@gmail.com>
2025-07-23 15:01:07 +0900

    Add files via upload

---
+++ lib/screens/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color gray = Color(0xFFBDBDBD);       // 일반 회색
+  static const Color green = Color(0xFF4CAF50);      // 조건 만족 체크 색
+  static const Color lightGray = Color(0xFFF0F0F0);  // 버튼 비활성화 배경 등
+  static const Color black = Colors.black;           // 텍스트용
+  static const Color white = Colors.white;           // 버튼 글자 등
+}
