705a79622a38cc1782860709a3da7bcd5f07b36c
ryuhochan0406 <yoo020406@naver.com>
2025-07-26 00:38:57 +0900

    추가 로그인 스크린

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:triprider/screens/Login/Email_input_screen.dart';
 import 'package:triprider/screens/Login/LoginScreen.dart';
 import 'package:triprider/screens/Login/WelcomeScreen.dart';
+import 'package:triprider/screens/Login/confirm_password_screen.dart';
+import 'package:triprider/screens/Login/nickname_input_screen.dart';
 import 'package:triprider/screens/Login/password_input_screen.dart';
 import 'package:triprider/screens/home/HomeScreen.dart';
 
 void main() {
-  runApp(MaterialApp(home: PasswordInputScreen()));
+  runApp(MaterialApp(home: NicknameInputScreen()));
 }
---
+++ lib/screens/Login/nickname_input_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:triprider/screens/Login/Email_input_screen.dart';
+import 'package:triprider/widgets/Login_Screen_Button.dart';
+import 'package:triprider/widgets/Next_Button_Widget_Child.dart';
+
+class NicknameInputScreen extends StatelessWidget {
+  const NicknameInputScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: Arrow_Back_ios_Pressed(),
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+
+      body: Column(
+        children: [
+          _ConfirmPassword(),
+
+          Expanded(child: SizedBox()),
+
+          LoginScreenButton(
+            T: 0,
+            B: 55,
+            L: 17,
+            R: 17,
+            child: Next_Widget_Child(),
+            color: Color(0XFFFF4E6B),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ConfirmPassword extends StatelessWidget {
+  const _ConfirmPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 35, left: 16, bottom: 25),
+          child: Text(
+            '닉네임을 입력해주세요.',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 16, bottom: 10),
+          child: Text('닉네임'),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: TextField(
+            style: TextStyle(fontSize: 20),
+            decoration: InputDecoration(
+              suffixIcon: Container(
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: Close_Button_Pressed,
+                  icon: Icon(Icons.close),
+                ),
+              ),
+
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
