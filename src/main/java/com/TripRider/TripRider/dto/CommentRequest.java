9a94babed23f677261371e205edaf088c301da61
ryuhochan0406 <yoo020406@naver.com>
2025-08-13 14:27:21 +0900

    Add files via upload
    fetch : comment(댓글) 파일 추가

---
+++ src/main/java/com/TripRider/TripRider/dto/CommentRequest.java
@@ -0,0 +1,10 @@
+// com.TripRider.TripRider.dto.CommentRequest.java
+package com.TripRider.TripRider.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CommentRequest {
+    private String content;
+}
---
+++ src/main/java/com/TripRider/TripRider/dto/CommentResponse.java
@@ -0,0 +1,14 @@
+// com.TripRider.TripRider.dto.CommentResponse.java
+package com.TripRider.TripRider.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @Builder @AllArgsConstructor @NoArgsConstructor
+public class CommentResponse {
+    private Long id;
+    private String user;          // 작성자 닉네임
+    private String content;
+    private LocalDateTime createdAt;
+    private boolean mine;         // 내가 쓴 댓글인지
+}
